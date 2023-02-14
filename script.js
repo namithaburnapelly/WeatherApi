5337718376ce15d7ef0c1bc2295660f02d17f9ba
Namitha B <102277118+namithaburnapelly@users.noreply.github.com>
2023-02-14 18:48:43 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-var date = [];
 function getDetails() {
     let xttp = new XMLHttpRequest();
     let city = document.getElementById("city").value;
     var dataList = []
+    var date = [];
+    var ch;
 
     let key = "07e5ee3702f491598157b15ceb669cb6"
     let url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}&units=metric`
 
     xttp.open("get", url, false);
     xttp.onreadystatechange = function () {
+
         if (this.status == 200 && this.readyState == 4) {
+
+            document.getElementById("table").innerHTML = '<canvas id = "myChart"></canvas>';
+            var ctx = document.getElementById('myChart');
+
             let data = JSON.parse(xttp.responseText);
             // console.log(data);
 
-            let { list } = data
+            let { list, city: { country } } = data
             // console.log(list)
 
             for (let i = 0; i < list.length; i = i + 8) {
@@ -23,40 +29,41 @@ function getDetails() {
             }
 
             let { main: { temp_max, temp_min } } = list[0]
-            getData(city, temp_min, temp_max);
-            getGraph(date, ...dataList)
+            getData(city, country, temp_min, temp_max);
+
+            ch = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: date,
+                    datasets: [{
+                        label: `Temperature`,
+                        data: dataList,
+                        borderWidth: 1,
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+
+        }
+        if (this.status == 404) {
+            document.getElementById("place").innerHTML = "city not found"
+            document.getElementById('min').innerHTML = ""
+            document.getElementById('max').innerHTML = ""
         }
     }
     xttp.send();
 
 }
 
-function getGraph(date, a, b, c, d, e) {
 
-    const ctx = document.getElementById('myChart');
-
-    ch = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: date,
-            datasets: [{
-                label: `Temperature`,
-                data: [a, b, c, d, e],
-                borderWidth: 1,
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
-}
-
-function getData(city, temp_min, temp_max) {
-    document.getElementById('place').innerHTML = `<i class="material-icons">place</i>  ${city}`
+function getData(city, country, temp_min, temp_max) {
+    document.getElementById('place').innerHTML = `<i class="material-icons">place</i>  ${city}, ${country}`
     document.getElementById('min').innerHTML = `Minimum Temp. is ${temp_min}`
     document.getElementById('max').innerHTML = `Maximum Temp. is ${temp_max}`
 }
